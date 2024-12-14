f32cc8d09ee13181fef4e2d71853f8c7ee3f78da
Thinh Ong <34956551+thinhong@users.noreply.github.com>
2024-12-14 23:01:43 +0700

    update paper url in software

--- code/gen_html.R
+++ code/gen_html.R
@@ -1,6 +1,6 @@
 gen_software_table <- function(cv_sheet) {
   z <- cv_sheet
-  out <- c()
+  out <- ""
   for (category in unique(z$category)) {
     out <- paste0(out, "<h3>", category, "</h3>\n\n")
     tmp <- z[z$category == category, ]
@@ -14,7 +14,7 @@ gen_software_table <- function(cv_sheet) {
                     "</strong><br>")
       out <- paste0(out, tmp$summary[i], "<br>")
       
-      # Bold "Ong T" in authors
+      # Bold "T Ong" in authors
       author_text <- gsub("T Ong", "<strong>T Ong</strong>", tmp$author[i], fixed = TRUE)
       out <- paste0(out, "<em>Authors: ", author_text, "</em><br>")
       
@@ -28,12 +28,14 @@ gen_software_table <- function(cv_sheet) {
           ifelse(tmp$lang1[i] == "bash", "bg-info", "bg-secondary")
         )
       )
-      out <- paste0(out,
-                    "<span class='badge ",
-                    lang1_badge,
-                    "'>",
-                    tmp$lang1[i],
-                    "</span> ")
+      out <- paste0(
+        out,
+        "<span class='badge rounded-pill ",
+        lang1_badge,
+        "'>",
+        tmp$lang1[i],
+        "</span> "
+      )
       
       if (!is.na(tmp$lang2[i])) {
         lang2_badge <- ifelse(
@@ -45,12 +47,24 @@ gen_software_table <- function(cv_sheet) {
             ifelse(tmp$lang2[i] == "bash", "bg-info", "bg-secondary")
           )
         )
-        out <- paste0(out,
-                      "<span class='badge ",
-                      lang2_badge,
-                      "'>",
-                      tmp$lang2[i],
-                      "</span> ")
+        out <- paste0(
+          out,
+          "<span class='badge rounded-pill ",
+          lang2_badge,
+          "'>",
+          tmp$lang2[i],
+          "</span> "
+        )
+      }
+      
+      # Add Paper badge if url_paper exists
+      if (!is.na(tmp$url_paper[i])) {
+        out <- paste0(
+          out,
+          "<a href='",
+          tmp$url_paper[i],
+          "' target='_blank'><span class='badge rounded-pill bg-secondary' style='float:right;'>Paper</span></a>"
+        )
       }
       
       out <- paste0(out, "</td>")
