1e78e7654cfca504f9158edd0efe725f025272bd
Thinh Ong <34956551+thinhong@users.noreply.github.com>
2024-12-21 18:44:32 +0700

    update cv

--- code/cv.R
+++ code/cv.R
@@ -7,13 +7,18 @@ make_italic <- function(text) {
   paste0("\\textit italicise{", text, " italicise}")
 }
 
+make_url <- function(text) {
+  paste0("\\url urlhere{", text, " urlhere}")
+}
+
 # A4 papersize is 210x297mm
 # Substract left=15mm and right=15mm we have 180mm left
-make_table <- function(data, colwidths = NULL, italic = F) {
+make_table <- function(data, colwidths = NULL, italic = F, header = NULL, longtable = F, newline = F, url = F, margin = NULL) {
   kable_obj <- kbl(data,
                    format = "latex",
                    booktabs = T,
-                   col.names = NULL) |>
+                   longtable = longtable,
+                   col.names = header) |>
     kable_styling(full_width = T)
   
   # Set column widths if user enter colwidths
@@ -31,6 +36,15 @@ make_table <- function(data, colwidths = NULL, italic = F) {
     kable_obj <- gsub(" italicise\\\\", "", kable_obj)
   }
   
+  if (newline) {
+    kable_obj <- gsub("\\\\textbackslash\\{\\}", "\\\\", kable_obj)
+  }
+  
+  if (url) {
+    kable_obj <- gsub("\\\\textbackslash\\{\\}", "\\\\", kable_obj)
+    kable_obj <- gsub(" urlhere\\\\", "", kable_obj)
+  }
+  
   # Remove top and bottom lines
-  gsub("\\\\toprule|\\\\bottomrule", "", kable_obj)
+  gsub("\\\\toprule|\\\\bottomrule|\\\\midrule", "", kable_obj)
 }
